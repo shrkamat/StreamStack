bfeb7ca8e2c3e1d83094ec28b5ec266f4739d122
Shrinivas Kamath <shrinivas.kamath@gmail.com>
2026-03-12 13:47:14 +0530

    able to playback sample mp4 & mpd, locally hosted mpd is failing (CORS)

--- app/web/player/src/App.tsx
+++ app/web/player/src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import ShakaPlayer from "./components/ShakaPlayer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* <ShakaPlayer src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" /> */}
+      <ShakaPlayer src="https://dash.akamaized.net/dash264/TestCasesUHD/2b/11/MultiRate.mpd" />
+    </>
+  );
 }
 
 export default App;
---
+++ app/web/player/src/components/ShakaPlayer.tsx
@@ -0,0 +1,67 @@
+import shaka from "shaka-player/dist/shaka-player.ui.js";
+import { useState, useRef, useEffect } from "react";
+
+// Define the shape of the props the component accepts
+interface ShakaPlayerProps {
+  src: string;
+}
+
+function ShakaPlayer({ src }: ShakaPlayerProps) {
+  const [videoSrc] = useState<string>(src);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const playerRef = useRef<shaka.Player | null>(null);
+
+  function onErrorEvent(event: any) {
+    console.error("Shaka Player error event:", event);
+  }
+
+  useEffect(() => {
+    console.log("Initializing Shaka Player with source:", videoSrc);
+
+    // Check if the browser supports the Shaka Player
+    if (shaka.Player.isBrowserSupported()) {
+      console.log("Shaka Player is supported in this browser.");
+
+      const player = new shaka.Player(videoRef.current);
+      player.attach(videoRef.current!);
+
+      player.addEventListener("error", onErrorEvent);
+
+      player
+        .load(videoSrc)
+        .then(() => {
+          console.log("The video has been loaded successfully.");
+        })
+        .catch((error) => {
+          console.error("Error loading video:", error);
+        });
+
+      // TODO: autoplay
+      // if (videoRef.current?.autoplay) {
+      //   player.play().catch((error: any) => {
+      //     console.error("Error playing video:", error);
+      //   });
+      // }
+      playerRef.current = player;
+    } else {
+      console.error("Shaka Player is not supported in this browser.");
+    }
+
+    return () => {
+      // cleanup here
+      console.log("Cleaning up Shaka Player resources.");
+      playerRef.current?.destroy().catch((error: any) => {
+        console.error("Error destroying Shaka Player:", error);
+      });
+    };
+  }, []);
+
+  return (
+    <div>
+      <h4>Shaka player, {videoSrc}</h4>
+      <video ref={videoRef} width="100%" controls autoPlay></video>
+    </div>
+  );
+}
+
+export default ShakaPlayer;
